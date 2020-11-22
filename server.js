aa4415c4bfca10d9f04954df2f5810bc1ffbbb26
Kanchan <kanchankandage@gmail.com>
2020-11-21 22:40:05 -0500

    added mongoose

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var mongoose = require('mongoose');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
@@ -16,7 +18,7 @@ app.get('/messages', (req, res)=>{
 })
 
 app.post('/messages', (req, res)=>{
-    console.log(req.body);
+    // console.log(req.body);
     messages.push(req.body);
     io.emit('message', req.body);
     res.sendStatus(200);
